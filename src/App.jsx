fc7a9ae77b8cba3000dfc22138171e2ce9e9ab55
TheEshwar <deshmukh.eshwar@gmail.com>
2023-12-21 18:43:53 +0530

    Added working redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+	return (
+		<>
+			<h1>Learnign RTK</h1>
+			<div>
+				<AddTodo />
+				<Todos />
+			</div>
+		</>
+	);
+}
+
+export default App;
